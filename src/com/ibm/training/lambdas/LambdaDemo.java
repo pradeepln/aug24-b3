ea76489b67831f6251fa446371d8363506e18b4b
Pradeep LN <pradeep.ln78@gmail.com>
2024-09-10 11:50:18 +0530

    after basic lambdas2

--- src/com/ibm/training/lambdas/LambdaDemo.java
+++ src/com/ibm/training/lambdas/LambdaDemo.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class LambdaDemo {
 
 	public static void main(String[] args) {
-		String[] words = new String[] {"this","is","a","series","of","weird","words","like","zebra","and","orion"};
+		String[] words = new String[] {"this","is","a","series","sardine","of","weird","words","like","zebra","and","orion"};
 		
 		System.out.println(Arrays.asList(words));
 		
@@ -13,8 +13,22 @@ public class LambdaDemo {
 		
 		//Arrays.sort(words, (s1,s2) -> {return s1.length() - s2.length();}); //step2
 		
-		Arrays.sort(words, (s1,s2) -> s1.length() - s2.length()); //step3
+		//Arrays.sort(words, (s1,s2) -> s1.length() - s2.length()); //step3
 		
+		
+		//Arrays.sort(words,(s1,s2) -> s2.length() - s1.length());
+		
+		//Arrays.sort(words,(s1,s2) -> s1.charAt(0) - s2.charAt(0));
+		
+		Arrays.sort(words,(s1,s2) -> {
+			if(s1.contains("e") && !s2.contains("e")) {
+				return -1;
+			}else if(!s1.contains("e") && s2.contains("e")) {
+				return 1;
+			}else {
+				return 0;
+			}
+		});
 		System.out.println(Arrays.asList(words));
 	}
 
